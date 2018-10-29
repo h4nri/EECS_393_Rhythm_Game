13fc7be5ca654a52955dadde3c819f46dc687f23
Henry Zheng <hxz256@case.edu>
2018-10-28 23:56:36 -0400

    Updated project name

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public void ToLevelSelect() {
+		SceneManager.LoadScene("Level Select");
+	}
+
+	public void ToSettings() {
+		SceneManager.LoadScene("Settings");
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+	public void PlayTwinkleTwinkleLittleStar() {
+		SceneManager.LoadScene("Twinkle Twinkle Little Star (Base)");
+	}
+}
