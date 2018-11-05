c44e69cc445ab234b9c1ff5b729f4f43c791f69f
Henry Zheng <hxz256@case.edu>
2018-11-05 08:24:27 -0500

    Moved Prefabs & Sprites into a new Resources folder, set-up testing framework for MonoBehaviours, and created test for GradeSprite

---
+++ Assets/Tests/GameSpriteTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using NUnit.Framework;
+using System.Collections;
+
+public class GameSpriteTest {
+
+    [Test]
+    public void SetSpriteCorrectlySetsSprites() {
+        var gradeSprite = new GameObject();
+        gradeSprite.AddComponent<GradeSprite>();
+        gradeSprite.AddComponent<Image>();
+
+        gradeSprite.GetComponent<GradeSprite>().SetSprite("Perfect");
+        Assert.AreSame(Resources.Load("Sprites/Score/Perfect/"), gradeSprite.GetComponent<Image>().sprite);
+
+        gradeSprite.GetComponent<GradeSprite>().SetSprite("Great");
+        Assert.AreSame(Resources.Load("Sprites/Score/Great/"), gradeSprite.GetComponent<Image>().sprite);
+
+        gradeSprite.GetComponent<GradeSprite>().SetSprite("Good");
+        Assert.AreSame(Resources.Load("Sprites/Score/Good/"), gradeSprite.GetComponent<Image>().sprite);
+
+        gradeSprite.GetComponent<GradeSprite>().SetSprite("Bad");
+        Assert.AreSame(Resources.Load("Sprites/Score/Bad/"), gradeSprite.GetComponent<Image>().sprite);
+
+        gradeSprite.GetComponent<GradeSprite>().SetSprite("Miss");
+        Assert.AreSame(Resources.Load("Sprites/Score/Miss/"), gradeSprite.GetComponent<Image>().sprite);
+    }
+}
